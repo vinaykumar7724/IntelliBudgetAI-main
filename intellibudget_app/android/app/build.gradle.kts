16226a0db8ed04aba616861d4dbb0099c8b4d47b
vinaykumar7724 <u.vinaykumar7724@gmail.com>
2026-04-29 22:43:46 +0530

    commit

--- intellibudget_app/android/app/build.gradle.kts
+++ intellibudget_app/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.intellibudget_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
