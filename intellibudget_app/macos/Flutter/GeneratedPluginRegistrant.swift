4417361084f24ae042815ce77415e3f8eaa762f4
vinaykumar7724 <u.vinaykumar7724@gmail.com>
2026-04-30 04:16:04 +0530

    filex

--- intellibudget_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ intellibudget_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import open_file_mac
 import share_plus
 import shared_preferences_foundation
 import speech_to_text
@@ -13,7 +12,6 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
