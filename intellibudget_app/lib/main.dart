e58d4f9bbe9c8c10cb21bbd74646ef6befc772f9
vinaykumar7724 <u.vinaykumar7724@gmail.com>
2026-04-30 03:06:01 +0530

    widget

--- intellibudget_app/lib/main.dart
+++ intellibudget_app/lib/main.dart
@@ -8,8 +8,12 @@ import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid) {
+    WebViewPlatform.instance = AndroidWebViewPlatform();
+  }
   runApp(const MyApp());
 }
 
