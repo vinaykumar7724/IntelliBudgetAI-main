5f3b1cbf7308ba9ff6cd40add17a79e0cc6b690e
vinaykumar7724 <u.vinaykumar7724@gmail.com>
2026-04-30 04:47:23 +0530

    pubspec deps

--- intellibudget_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ intellibudget_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import share_plus
-import shared_preferences_foundation
 import speech_to_text
-import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
