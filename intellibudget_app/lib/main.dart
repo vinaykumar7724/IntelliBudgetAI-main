c31520e2e8b777c1c5704fd49bb3b3588234f4c0
vinaykumar7724 <u.vinaykumar7724@gmail.com>
2026-04-30 03:17:22 +0530

    s

--- intellibudget_app/lib/main.dart
+++ intellibudget_app/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:dio/dio.dart';
 import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
-
+import 'package:flutter/foundation.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
